76c133145ec18b50e4f6088584af1f684195110f
Mateo <radmanmateo@gmail.com>
2021-01-26 00:57:42 +0100

    Change Scraper.py

--- Engine/Scraper.py
+++ Engine/Scraper.py
@@ -1,11 +1,41 @@
+from urllib import request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from time import sleep
+from random import randint
+from fake_useragent import UserAgent
 
-LINK = ''
-def return_page_title(LINK):
-    page_link = urlopen(LINK).read()
-    soup = BeautifulSoup(page_link, 'html.parser')
-    return soup.title.string
+import requests
+from requests import utils
+
+# LINK = input('')
+# def return_page_title(LINK):
+#     page_link = urlopen(LINK).read()
+#     soup = BeautifulSoup(page_link, 'html.parser')
+#     return soup.title.string
+
+def return_data_from_a_website():
+    title = []
+    price = []
+    sqm = []
+    location = []
+
+    ua = UserAgent()
+    pages = range(1,11)
+    headers = utils.default_headers()
+    headers.update({'User-Agent': ua.ie})
+
+    for page in pages:
+        page_request = requests.get("https://www.njuskalo.hr/prodaja-kuca?page={}".format(page), headers)
+        soup = BeautifulSoup(page_request.text, 'html.parser')
+        filename = "njuskalo-{}.html".format(page)
+        with open(filename, 'a+') as f:
+            f.write(str(soup))
+        sleep(randint(2,10))
+
+return_data_from_a_website()
+
+
 
 
 
