ce1274c37e96711d3dc91a583ff788b7eeb0457d
Mateo <radmanmateo@gmail.com>
2021-01-27 23:20:15 +0100

    Parser changes

--- Engine/Scraper.py
+++ Engine/Scraper.py
@@ -8,10 +8,12 @@ from fake_useragent import UserAgent
 from time import sleep
 import re
 
-data = {}
+
 
 
 def get_data(n):
+    data = {}
+    count = 0
     for page in range(1,n):
         try:
             page_request = requests.get("https://www.njuskalo.hr/prodaja-kuca?page={}".format(page),headers = {"User-Agent":UserAgent().random})
@@ -19,17 +21,17 @@ def get_data(n):
             prices = soup.find_all("strong", attrs={"class":"price price--hrk"})
             areas =  soup.find_all("div",attrs={"class":"entity-description-main"})
             for i in range(len(prices)):
+                count += 1
                 price = prices[i].get_text().replace("kn",'').replace('.','').strip()
                 price = int(price)
             
                 loc = str(areas[i])
                 loc = loc.split("</span>")[1].split(",")[0]
-            
                 area = areas[i].find(string=re.compile("Stambena"))
                 area = area.strip().replace("Stambena površina: ",'').replace(' m2','') 
                 area = float(area)
-                data[str(i)] = {'price': price, 'location': loc, 'area': area}
-            sleep(random.randint(4,10))
+                data[str(count)] = {'price': price, 'location': loc, 'area': area}
+            sleep(random.randint(2,6))
         except Exception:
             break
     with open('data.json', 'w+') as json_file:
@@ -37,4 +39,4 @@ def get_data(n):
 
 
 
-get_data(399)
+get_data(3)
