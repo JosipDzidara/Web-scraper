c50a4b4aaeba068b4c0dd9631f6c9d6c6fe05e36
Mateo <radmanmateo@gmail.com>
2021-02-13 23:31:54 +0100

    Refactor Django project and start building GUI

--- ScraperGUI/ScraperGUI/urls.py
+++ ScraperGUI/ScraperGUI/urls.py
@@ -1,4 +1,4 @@
-"""WebScraper URL Configuration
+"""ScraperGUI URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("GUI.urls"))
 ]
--- WebScraper/GUI/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.get_name, name="Search web")
-]
