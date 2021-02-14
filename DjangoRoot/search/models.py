2ff142aaa303bf4ac9581e760e1142324c42d664
Mateo <radmanmateo@gmail.com>
2021-02-14 10:51:39 +0100

    Add navbar, and start developing the index page.

--- DjangoRoot/search/models.py
+++ DjangoRoot/search/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Search(models.Model):
+    search = models.CharField(max_length=255, blank=False, null=False)
+    time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Search'
+        verbose_name_plural = 'Searches'
