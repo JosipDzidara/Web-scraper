1e4ef076724e77c9aa97ae81d142b8d386fd4775
Mateo <radmanmateo@gmail.com>
2021-02-14 19:32:13 +0100

    Add forms.py to search app

---
+++ DjangoRoot/search/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class ml_input(forms.Form):
+    county = forms.ChoiceField(choices=[('c1','SB'),('c2','ZG')])
+    n_room = forms.IntegerField(label='Number of rooms')
+    sqr_out = forms.IntegerField(label='Outer area')
+    sqr_in = forms.IntegerField(label='Inner area')
+    see_view = forms.ChoiceField(choices=[('c1','Da'),('c2','Ne')])
+
