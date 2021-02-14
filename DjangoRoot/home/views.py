220f0e25ef342b554cdb05849427c2433690de48
Mateo <radmanmateo@gmail.com>
2021-02-14 19:16:27 +0100

    Add forms and get user input for the estimation of housing prices.

--- DjangoRoot/home/views.py
+++ DjangoRoot/home/views.py
@@ -1,4 +1,3 @@
 from django.shortcuts import render
 
-def index_view(request):
-    pass
+# Create your views here.
--- DjangoRoot/search/views.py
+++ DjangoRoot/search/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import ml_input
 
-def home(request):
-    return render(request, 'search/base.html')
+
+# Create your views here.
+def contact(request):
+    if request.method == "POST":
+        form = ml_input(request.POST)
+        if form.is_valid():
+            county = form.cleaned_data['county']
+            n_room = form.cleaned_data['n_room']
+            sqr_out = form.cleaned_data['sqr_out']
+            sqr_in = form.cleaned_data['sqr_in']
+            see_view = form.cleaned_data['see_view']
+            print(county, n_room)
+            result = [county, n_room, sqr_out, sqr_in, see_view]
+
+            return render(request, 'search/result.html', {'result': result})
+
+    form = ml_input()
+    return render(request, 'search/form.html', {'form': form})
+
