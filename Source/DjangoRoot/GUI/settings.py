141b700950c989a9253ba7669cdedef142eaff8e
Mateo <radmanmateo@gmail.com>
2021-02-22 22:58:48 +0100

    Introduce Django crispy forms

--- Source/DjangoRoot/GUI/settings.py
+++ Source/DjangoRoot/GUI/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'uni_form'
+
--- Source/DjangoRoot/search/forms.py
+++ Source/DjangoRoot/search/forms.py
@@ -1,9 +1,36 @@
 from django import forms
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Field
+from crispy_forms.bootstrap import FormActions
+
+
+counties_list = ['Bjelovarsko-bilogorska', 'Brodsko-posavska', 'Dubrovačko-neretvanska',
+                 'Grad Zagreb', 'Istarska', 'Karlovačka', 'Koprivničko-križevačka',
+                 'Krapinsko-zagorska', 'Ličko-senjska', 'Međimurska',
+                 'Osječko-baranjska', 'Požeško-slavonska', 'Primorsko-goranska',
+                 'Sisačko-moslavačka', 'Splitsko-dalmatinska', 'Varaždinska',
+                 'Virovitičko-podravska', 'Vukovarsko-srijemska', 'Zadarska',
+                 'Zagrebačka', 'Šibensko-kninska']
+county_choices = set()
+for county in counties_list:
+    county_choices.add((county, county))
+
+
 class MachineLearningForm(forms.Form):
-    county = forms.ChoiceField(choices=(('Bjelovarsko-bilogorska','Bjelovarsko-bilogorska'),('Brodsko-posavska','Brodsko-posavska')))
+    county = forms.ChoiceField(choices=county_choices, widget=forms.Select)
     n_room = forms.IntegerField(label='Number of rooms')
     sqr_out = forms.IntegerField(label='Outer area')
     sqr_in = forms.IntegerField(label='Inner area')
-    see_view = forms.ChoiceField(choices=((1 ,'Da'),(0 ,'Ne')))
+    sea_view = forms.ChoiceField(choices=((1, 'Da'), (0, 'Ne')), widget=forms.RadioSelect)
 
+    # Uni-form
+    helper = FormHelper()
+    helper.form_class = 'form-horizontal'
+    helper.layout = Layout(
+        'county',
+        Field('n_room', style="background: #FAFAFA; padding: 10px;"),
+        Field('sqr_out', style="background: #FAFAFA; padding: 10px;"),
+        Field('sqr_in', style="background: #FAFAFA; padding: 10px;"),
+        'sea_view'
+    )
