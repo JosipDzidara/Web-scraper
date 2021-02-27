8664fd5def5bb8c2c9d07ad5499a02143678741f
Mateo <radmanmateo@gmail.com>
2021-02-27 20:13:14 +0100

    Start developing front-end

--- Source/DjangoRoot/search/forms.py
+++ Source/DjangoRoot/search/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Field, ButtonHolder, Fieldset
-from crispy_forms.bootstrap import FormActions
+from django.core.validators import MinValueValidator
 
 counties_list = ['Bjelovarsko-bilogorska', 'Brodsko-posavska', 'Dubrovačko-neretvanska',
                  'Grad Zagreb', 'Istarska', 'Karlovačka', 'Koprivničko-križevačka',
@@ -18,12 +18,14 @@ for county in counties_list:
 
 class MachineLearningForm(forms.Form):
     county = forms.ChoiceField(choices=county_choices, widget=forms.Select)
-    n_room = forms.IntegerField(label='Number of rooms')
-    sqr_out = forms.IntegerField(label='Outer area')
-    sqr_in = forms.IntegerField(label='Inner area')
-    sea_view = forms.ChoiceField(choices=((1, 'Da'), (0, 'Ne')), widget=forms.RadioSelect)
+    n_room = forms.IntegerField(label='Number of rooms', validators=[MinValueValidator(1, message='Minimum number of '
+                                                                                                  'rooms is 1.')])
+    sqr_out = forms.IntegerField(label='Outer area', validators=[MinValueValidator(1, message='Minimum size of '
+                                                                                                  'outer area is 1.')])
+    sqr_in = forms.IntegerField(label='Inner area', validators=[MinValueValidator(1, message='Minimum size of '
+                                                                                                  'iner area is 1.')])
+    sea_view = forms.ChoiceField(choices=((1, 'Yes'), (0, 'No')), widget=forms.RadioSelect)
 
-    # Uni-form
     helper = FormHelper()
     helper.form_class = 'form-horizontal'
     helper.layout = Layout(
@@ -38,3 +40,4 @@ class MachineLearningForm(forms.Form):
             Submit('submit', 'Submit', css_class='button white')
         )
     )
+
